22284338964da62d7feb5f050cc151ee1c1e3b3e
ryuichi-works <ryu1.works@gmail.com>
2023-11-30 21:03:24 +0900

    add: gutページ、pcサイズのレイアウトの作成
    gut詳細画面へのリンクとそれに伴って詳細画面を簡易実装した

---
+++ src/pages/guts/[id]/index.tsx
@@ -0,0 +1,9 @@
+const Gut = () =>{
+  return (
+    <>
+      <h1>ストリング詳細</h1>
+    </>
+  );
+}
+
+export default Gut;
--- src/pages/guts/index.tsx
+++ src/pages/guts/index.tsx
@@ -3,6 +3,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import axios from "@/lib/axios";
 import { AuthContext } from "@/context/AuthContext";
+import Link from "next/link";
 
 import AuthCheck from "@/components/AuthCheck";
 import TextUnderBar from "@/components/TextUnderBar";
@@ -33,40 +34,40 @@ const GutList = () => {
     <>
       <AuthCheck>
         {isAuth && (
-          // <h1>ストリング一覧ページ</h1>
-          <div className="container">
+          <div className="container mx-auto">
             <div className="text-center mb-6">
-              <PrimaryHeading text="Strings" className="text-[18px] h-[20px]" />
+              <PrimaryHeading text="Strings" className="text-[18px] h-[20px] md:text-[20px] md:h-[22px]" />
             </div>
 
-            <div className="flex justify-center mb-6">
-              <button className="text-white text-[14px] w-[264px] h-8 rounded  bg-sub-green">検索</button>
+            <div className="flex justify-center mb-6 md:justify-end md:w-[100%] md:max-w-[768px] md:mx-auto">
+              <button className="text-white text-[14px] w-[264px] h-8 rounded  bg-sub-green md:w-[80px] md:h-[32px]">検索</button>
             </div>
 
             {/* ガットセクション */}
-            <div>
-              <div className="w-[100%] max-w-[320px] mx-auto">
+            <div className="">
+              <div className="w-[100%] max-w-[320px] mx-auto md:max-w-[768px] md:flex md:flex-wrap md:justify-between ">
                 {/* ガット */}
                 {guts && guts.map(gut => (
-                  <div key={gut.name_ja} className="flex  mb-6 hover:opacity-80 hover:cursor-pointer">
-                    <div className="w-[120px] mr-6">
-                      {gut.gut_image.file_path
-                        ? <img src={`${baseImagePath}${gut.gut_image.file_path}`} alt="ストリング画像" className="w-[120px] h-[120px]" />
-                        : <img src={`${baseImagePath}images/users/defalt_user_image.jpg`} alt="ストリング画像" className="w-[120px] h-[120px]" />
-                      }
-                    </div>
+                  <Link href={`/guts/${gut.id}`} key={gut.id} className="block hover:opacity-80 hover:cursor-pointer md:w-[100%] md:max-w-[360px]">
+                    <div className="flex  mb-6 hover:opacity-80 hover:cursor-pointer md:w-[100%] md:max-w-[360px]">
+                      <div className="w-[120px] mr-6">
+                        {gut.gut_image.file_path
+                          ? <img src={`${baseImagePath}${gut.gut_image.file_path}`} alt="ストリング画像" className="w-[120px] h-[120px]" />
+                          : <img src={`${baseImagePath}images/users/defalt_user_image.jpg`} alt="ストリング画像" className="w-[120px] h-[120px]" />
+                        }
+                      </div>
 
-                    <div className="w-[100%] max-w-[160px]">
-                      <p className="text-[14px] mb-2">{gut.maker.name_ja}</p>
-                      <p className="text-[16px] mb-2">{gut.name_ja}</p>
-                      <TextUnderBar className="w-[100%] max-w-[160px]" />
+                      <div className="w-[100%] max-w-[160px] md:max-w-[216px]">
+                        <p className="text-[14px] mb-2 md:text-[16px]">{gut.maker.name_ja}</p>
+                        <p className="text-[16px] mb-2 md:text-[18px]">{gut.name_ja}</p>
+                        <TextUnderBar className="w-[100%] max-w-[160px] md:max-w-[216px]" />
+                      </div>
                     </div>
-                  </div>
+                  </Link>
                 ))}
               </div>
             </div>
           </div>
-
         )}
       </AuthCheck>
     </>
