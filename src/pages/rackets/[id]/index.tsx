8d740718c0607b3d5a3230d03ad1b71955c0e4f5
ryuichi-works <ryu1.works@gmail.com>
2023-12-02 06:52:13 +0900

    add: その他のラケット情報を取得し表示、linkクリック後はその情報を表示するようにした
    idをuseStateで持たせることによってその他のストリングをクリックした時このidが更新されuseEffectの依存配列に指定して更新されるようにした

--- src/pages/rackets/[id]/index.tsx
+++ src/pages/rackets/[id]/index.tsx
@@ -6,34 +6,49 @@ import PrimaryHeading from "@/components/PrimaryHeading";
 import axios from "@/lib/axios";
 import { useRouter } from "next/router";
 import TextUnderBar from "@/components/TextUnderBar";
+import Link from "next/link";
+
 import { firstLetterToUpperCase } from "@/modules/firstLetterToUpperCase";
 
 const RacketShow = () => {
   const router = useRouter();
 
-  const id = router.query.id;
+  const [id, setId] = useState(router.query.id)
 
   const { isAuth, user } = useContext(AuthContext);
 
   const [racket, setRacket] = useState<Racket>();
   console.log(racket);
 
+  const [otherRackets, setOtherRackets] = useState<Racket[]>();
+  console.log(otherRackets);
+
+  const otherRacketsCount = 5;
+
   const baseImagePath = process.env.NEXT_PUBLIC_BACKEND_URL + '/storage/';
 
   useEffect(() => {
     if (user.id) {
       const getRacket = async () => {
-        // await axios.get('api/rackets').then(res => {
         await axios.get(`api/rackets/${id}`).then(res => {
           setRacket(res.data);
         })
       }
 
+      const getOtherGuts = async () => {
+        await axios.get(`api/rackets/${id}/others`, {
+          params: { count: otherRacketsCount }
+        }).then(res => {
+          setOtherRackets(res.data);
+        })
+      }
+
       getRacket();
+      getOtherGuts();
     } else {
       router.push('/users/login');
     }
-  }, [])
+  }, [id])
 
   return (
     <>
@@ -64,7 +79,7 @@ const RacketShow = () => {
             </div>
 
             {/* ラケット情報セクション */}
-            <div>
+            <div className="mb-[64px]">
               <div className="w-[100%] max-w-[320px] mx-auto">
                 <table className="flex justify-center w-[100%] max-w-[320px] ">
                   <thead className="w-[100%] max-w-[160px]">
@@ -93,8 +108,32 @@ const RacketShow = () => {
               </div>
             </div>
 
-          </div>
+            {/* otherラケットセクション */}
+            <div className="">
+              <p className="text-[14px] w-[100%] max-w-[320px] mx-auto mb-2 md:max-w-[768px] md:mb-[16px]">その他のラケット</p>
+              <div className="w-[100%] max-w-[320px] mx-auto md:max-w-[768px] md:flex md:flex-wrap md:justify-between ">
+                {/* ラケット */}
+                {otherRackets && otherRackets.map(otherRacket => (
+                  <Link href={`/rackets/${otherRacket.id}`} key={otherRacket.id} onClick={() => setId(`${otherRacket.id}`)} className="block hover:opacity-80 hover:cursor-pointer md:w-[100%] md:max-w-[360px]">
+                    <div className="flex  mb-6 hover:opacity-80 hover:cursor-pointer md:w-[100%] md:max-w-[360px]">
+                      <div className="w-[120px] mr-6">
+                        {otherRacket.racket_image.file_path
+                          ? <img src={`${baseImagePath}${otherRacket.racket_image.file_path}`} alt="ストリング画像" className="w-[120px] h-[160px]" />
+                          : <img src={`${baseImagePath}images/users/defalt_user_image.jpg`} alt="ストリング画像" className="w-[120px] h-[160px]" />
+                        }
+                      </div>
 
+                      <div className="w-[100%] max-w-[176px] pt-4 md:max-w-[216px]">
+                        <p className="text-[14px] mb-2 pl-2 md:text-[16px]">{firstLetterToUpperCase(otherRacket.maker.name_en)}</p>
+                        <p className="text-[16px] text-center mb-2 md:text-[18px]">{otherRacket.name_ja}</p>
+                        <TextUnderBar className="w-[100%] max-w-[176px] md:max-w-[216px]" />
+                      </div>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
         )}
       </AuthCheck>
     </>
