e6c30a1fa7f02ead629e9f39f487548210165fa9
ryuichi-works <ryu1.works@gmail.com>
2023-12-02 07:52:44 +0900

    add: racket詳細ページ、pcサイズのレイアウトの実装

--- src/pages/rackets/[id]/index.tsx
+++ src/pages/rackets/[id]/index.tsx
@@ -56,52 +56,53 @@ const RacketShow = () => {
         {isAuth && (
           // <h1>ラケット詳細ページ</h1>
           <div className="container mx-auto">
-            <div className="text-center mb-6">
+            <div className="text-center mb-6 md:mb-[48px]">
               <PrimaryHeading text="Rackets" className="text-[18px] h-[20px] md:text-[20px] md:h-[22px]" />
             </div>
 
             {/* ラケットセクション */}
-            <div className="w-[100%] max-w-[320px] mx-auto mb-8">
-              <div className="flex md:w-[100%] md:max-w-[360px]">
-                <div className="w-[120px] mr-6">
+            <div className="w-[100%] max-w-[320px] mx-auto mb-8 md:max-w-[400px] md:mb-[64px]">
+              <div className="flex md:w-[100%] md:max-w-[400px]">
+                <div className="w-[120px] mr-6 md:w-[160px] md:mr-8">
                   {racket?.racket_image.file_path
-                    ? <img src={`${baseImagePath}${racket.racket_image.file_path}`} alt="ストリング画像" className="w-[120px] h-[160px]" />
-                    : <img src={`${baseImagePath}images/users/defalt_user_image.jpg`} alt="ストリング画像" className="w-[120px] h-[160px]" />
+                    ? <img src={`${baseImagePath}${racket.racket_image.file_path}`} alt="ストリング画像" className="w-[120px] h-[160px] md:w-[160px] md:h-[200px]" />
+                    : <img src={`${baseImagePath}images/users/defalt_user_image.jpg`} alt="ストリング画像" className="w-[120px] h-[160px] md:w-[160px] md:h-[200px]" />
                   }
                 </div>
 
-                <div className="w-[100%] max-w-[176px] pt-4 md:max-w-[216px]">
+                <div className="w-[100%] max-w-[176px] pt-4 md:max-w-[208px]">
                   <p className="text-[14px] mb-2 pl-2 md:text-[16px]">{racket && firstLetterToUpperCase(racket.maker.name_en)}</p>
                   <p className="text-[16px] text-center mb-2 md:text-[18px]">{racket?.name_ja}</p>
-                  <TextUnderBar className="w-[100%] max-w-[176px] md:max-w-[216px]" />
+                  <TextUnderBar className="w-[100%] max-w-[176px] md:max-w-[208px]" />
                 </div>
               </div>
             </div>
 
             {/* ラケット情報セクション */}
-            <div className="mb-[64px]">
-              <div className="w-[100%] max-w-[320px] mx-auto">
-                <table className="flex justify-center w-[100%] max-w-[320px] ">
-                  <thead className="w-[100%] max-w-[160px]">
+            <div className="mb-[64px] md:mb-[128px]">
+              <div className="w-[100%] max-w-[320px] mx-auto md:max-w-[768px]">
+                <table className="flex justify-center w-[100%] max-w-[320px] md:max-w-[768px] ">
+                  <thead className="w-[100%] max-w-[160px] md:max-w-[384px]">
                     <tr className="flex flex-col text-left ">
-                      <th className="font-normal text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green">メーカー</th>
-                      <th className="font-normal text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green">ヘッドサイズ</th>
-                      <th className="font-normal text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green">重さ</th>
-                      <th className="font-normal text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green">長さ</th>
-                      <th className="font-normal text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green">バランス</th>
-                      <th className="font-normal text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green">ストリングパターン</th>
-                      <th className="font-normal text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-y border-sub-green">フレーム厚</th>
+                      <th className="font-normal text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green md:text-[16px] md:min-h-[50px] md:leading-[50px] md:pl-6">メーカー</th>
+                      <th className="font-normal text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green md:text-[16px] md:min-h-[50px] md:leading-[50px] md:pl-6">ヘッドサイズ</th>
+                      <th className="font-normal text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green md:text-[16px] md:min-h-[50px] md:leading-[50px] md:pl-6">重さ</th>
+                      <th className="font-normal text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green md:text-[16px] md:min-h-[50px] md:leading-[50px] md:pl-6">長さ</th>
+                      <th className="font-normal text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green md:text-[16px] md:min-h-[50px] md:leading-[50px] md:pl-6">バランス</th>
+                      <th className="font-normal text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green md:text-[16px] md:min-h-[50px] md:leading-[50px] md:pl-6">ストリングパターン</th>
+                      <th className="font-normal text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-y border-sub-green md:text-[16px] md:min-h-[50px] md:leading-[50px] md:pl-6">フレーム厚</th>
                     </tr>
                   </thead>
-                  <tbody className="w-[100%] max-w-[320px]">
+
+                  <tbody className="w-[100%] max-w-[320px] md:max-w-[384px]">
                     <tr className="flex flex-col text-left">
-                      <td className="text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green">{racket?.maker.name_ja}</td>
-                      <td className="text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green">645 cm² / 100 in²</td>
-                      <td className="text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green">310g</td>
-                      <td className="text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green">685mm/27in</td>
-                      <td className="text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green">330mm +/- 7mm</td>
-                      <td className="text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green">16/19</td>
-                      <td className="text-[14px] min-h-[16px] leading-[32px] p-[0px] pl-1 border-y border-sub-green">23/26/23mm</td>
+                      <td className="text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green md:text-[16px] md:min-h-[50px] md:leading-[50px]">{racket?.maker.name_ja}</td>
+                      <td className="text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green md:text-[16px] md:min-h-[50px] md:leading-[50px]">未登録</td>
+                      <td className="text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green md:text-[16px] md:min-h-[50px] md:leading-[50px]">未登録</td>
+                      <td className="text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green md:text-[16px] md:min-h-[50px] md:leading-[50px]">未登録</td>
+                      <td className="text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green md:text-[16px] md:min-h-[50px] md:leading-[50px]">未登録</td>
+                      <td className="text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-t border-sub-green md:text-[16px] md:min-h-[50px] md:leading-[50px]">未登録</td>
+                      <td className="text-[14px] min-h-[32px] leading-[32px] p-[0px] pl-1 border-y border-sub-green md:text-[16px] md:min-h-[50px] md:leading-[50px]">未登録</td>
                     </tr>
                   </tbody>
                 </table>
