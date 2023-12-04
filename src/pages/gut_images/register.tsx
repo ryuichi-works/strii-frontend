c3000dfa3e2198379cf71215c064ab4bb9068aba
ryuichi-works <ryu1.works@gmail.com>
2023-12-04 21:58:00 +0900

    add: フォーム、ファイル選択欄の設置
    いったんファイルを選択する機能まで実装し、この後で画像トリミング機能を仮実装する

--- src/pages/gut_images/register.tsx
+++ src/pages/gut_images/register.tsx
@@ -1,16 +1,72 @@
 import AuthCheck from "@/components/AuthCheck";
 import { AuthContext } from "@/context/AuthContext";
 import { NextPage } from "next";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+// import ModalSpSize from "@/components/ModalSpSize";
+import CloseIcon from "@/components/CloseIcon";
+import axios from "@/lib/axios";
 
 const GutImageRegister: NextPage = () => {
   const { isAuth, user } = useContext(AuthContext);
+
+  const [imageFile, setImageFile] = useState<File | null>();
+  console.log('imageFile', imageFile);
+
+  const onChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files
+    if (files && files[0]) {
+      setImageFile(files[0])
+    }
+    console.log(imageFile);
+  }
+
+  const csrf = async () => await axios.get('/sanctum/csrf-cookie');
+  
+
+  // const [showingModal, setShowingModal] = useState<boolean>(false);
+
+  // const toggleModal = () => {
+  //   setShowingModal(prev => !prev);
+  // }
+  
   return (
     <>
       <AuthCheck>
         {isAuth && (
           <>
             <h1>ストリング画像登録</h1>
+
+            <div className="container mx-auto">
+
+
+              <div>
+                <form action="">
+                  {/* <label htmlFor="gut_image_file"></label>
+                  <input type="file" name="gut_image" id="gut_image" onChange={} /> */}
+
+                  <div className="flex flex-col mb-6">
+                      <label htmlFor="gut_image_file">ストリング画像ファイル</label>
+                      <input type="file" name="gut_image_file" accept=".jpg, .jpeg, .png" onChange={onChangeFile} className="h-8" />
+                      {/* {errors.file.length !== 0 &&
+                        errors.file.map((message, i) => <p key={i} className="text-red-400">{message}</p>)
+                      } */}
+                    </div>
+                </form>
+              </div>
+
+              {/* //モーダル */}
+              {/* <div className="h-screen bg-sub-gray bg-opacity-30">
+                <div className="pt-6">
+                  <div className="flex justify-end w-[100%] max-w-[320px] mx-auto">
+                    <span className="inline-block" onClick={toggleModal}>
+                      <CloseIcon size={32} />
+                    </span>
+                  </div>
+                </div>
+              </div> */}
+
+            </div>
+            
           </>
         )}
       </AuthCheck>
