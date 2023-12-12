78c789f28245017b1aaecd80cbbe8cdfbc0f55ea
ryuichi-works <ryu1.works@gmail.com>
2023-12-13 04:48:23 +0900

    add:　gut登録ページ：gut登録機能の実装（gut_iamge検索機能付き）
    maker情報も取ってくるため、型RacketImagにmakerを追加してある

--- src/pages/rackets/register.tsx
+++ src/pages/rackets/register.tsx
@@ -1,5 +1,4 @@
-import type { Maker } from "../users/[id]/profile";
-
+import type { Maker, RacketImage } from "../users/[id]/profile";
 import AuthAdminCheck from "@/components/AuthAdminCheck";
 import PrimaryHeading from "@/components/PrimaryHeading";
 import { AuthContext } from "@/context/AuthContext";
@@ -10,41 +9,37 @@ import { useRouter } from "next/router";
 import React, { useContext, useEffect, useState } from "react";
 import { IoClose } from "react-icons/io5";
 
-export type GutImage = {
-  id: number,
-  file_path: string,
-  title: string,
-  created_at: string,
-  updated_at: string,
-  maker_id: number,
-  maker: Maker
-}
 
 const RacketRegister: NextPage = () => {
   const router = useRouter();
 
-  //gut登録用のデータstate
+  //racket登録用のデータstate
   const [inputNameJa, setInputNameJa] = useState<string>('');
 
   const [inputNameEn, setInputNameEn] = useState<string>('');
 
-  const [gutMakerId, setGutMakerId] = useState<number | null>(null);
+  const [racketMakerId, setRacketMakerId] = useState<number | null>(null);
 
   const [needPostingImage, setNeedPostingImage] = useState<boolean>(true);
 
-  const [gutImageId, setGutImageId] = useState<number>();
+  const [racketImageId, setRacketImageId] = useState<number>();
 
   const [makers, setMakers] = useState<Maker[]>();
 
-  const [selectedGutImage, setSelectedGutImage] = useState<GutImage>();
+  const [selectedRacketImage, setSelectedRacketImage] = useState<RacketImage>();
+  
+  console.log('inputNameJa', inputNameJa)
+  console.log('inputNameEn', inputNameEn)
+  console.log('racketMakerId', racketMakerId)
+  console.log('needPostingImage', needPostingImage)
+  console.log('racketImageId', racketImageId)
 
-``
   //検索関連のstate
   const [inputSearchWord, setInputSearchWord] = useState<string>('');
 
   const [inputSearchMaker, setInputSearchMaker] = useState<number | null>();
 
-  const [searchedGutImages, setSearchedGutImages] = useState<GutImage[]>();
+  const [searchedRacketImages, setSearchedRacketImages] = useState<RacketImage[]>();
 
   //モーダルの開閉に関するstate
   const [modalVisibilityClassName, setModalVisibilityClassName] = useState<string>('opacity-0 scale-0');
@@ -54,22 +49,22 @@ const RacketRegister: NextPage = () => {
   const baseImagePath = process.env.NEXT_PUBLIC_BACKEND_URL + '/storage/'
 
   useEffect(() => {
-    const getGutList = async () => {
+    const getMakerList = async () => {
       await axios.get('api/makers').then(res => {
         setMakers(res.data);
       })
     }
 
-    getGutList();
+    getMakerList();
   }, [])
 
   const onChangeMaker = (e: React.ChangeEvent<HTMLSelectElement>) => {
     if (e.target.value === '未選択') {
-      setGutMakerId(null);
+      setRacketMakerId(null);
       return
     };
 
-    setGutMakerId(Number(e.target.value));
+    setRacketMakerId(Number(e.target.value));
   }
 
   const onChangeInputSearchMaker = (e: React.ChangeEvent<HTMLSelectElement>) => {
@@ -102,18 +97,18 @@ const RacketRegister: NextPage = () => {
 
   const [errors, setErrors] = useState<Errors>(initialErrorVals);
 
-  //gutImage検索
-  const searchGutImages = async (e: React.FormEvent<HTMLFormElement>) => {
+  //racketImage検索
+  const searchRacketImages = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     try {
-      await axios.get('api/gut_images/search', {
+      await axios.get('api/racket_images/search', {
         params: {
           several_words: inputSearchWord,
           maker: inputSearchMaker
         }
       }).then((res) => {
-        setSearchedGutImages(res.data);
+        setSearchedRacketImages(res.data);
       })
 
       console.log('検索完了しました')
@@ -123,42 +118,42 @@ const RacketRegister: NextPage = () => {
   }
 
 
-  const selectImage = (gutImage: GutImage) => {
-    setSelectedGutImage(gutImage);
-    setGutImageId(gutImage.id);
+  const selectImage = (racketImage: RacketImage) => {
+    setSelectedRacketImage(racketImage);
+    setRacketImageId(racketImage.id);
     closeModal();
   }
 
   //gut登録処理関連
   const csrf = async () => await axios.get('/sanctum/csrf-cookie');
 
-  const registerGut = async (e: React.FormEvent<HTMLFormElement>) => {
+  const registerRacket = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const registerData = {
       name_ja: inputNameJa,
       name_en: inputNameEn,
-      maker_id: gutMakerId,
-      image_id: gutImageId,
+      maker_id: racketMakerId,
+      image_id: racketImageId,
       need_posting_image: needPostingImage,
     }
 
     await csrf();
 
-    await axios.post('api/guts', registerData, {
+    await axios.post('api/rackets', registerData, {
       headers: {
         'X-Xsrf-Token': Cookies.get('XSRF-TOKEN'),
       }
     }).then(res => {
-      console.log('ストリングを新規登録しました。');
+      console.log('ラケットを新規登録しました。');
 
-      router.push('/guts');
+      router.push('/rackets');
     }).catch((e) => {
       const newErrors = { ...initialErrorVals, ...e.response.data.errors };
 
       setErrors(newErrors);
 
-      console.log('ストリングの登録に失敗しました');
+      console.log('ラケットの登録に失敗しました');
     })
   }
 
@@ -175,9 +170,9 @@ const RacketRegister: NextPage = () => {
               </div>
 
               <div className="w-[100%] max-w-[320px] md:max-w-[380px] mx-auto flex flex-col md:justify-center">
-                <form action="" onSubmit={(e) => registerGut(e)}>
+                <form action="" onSubmit={(e) => registerRacket(e)}>
                   <div className="mb-6">
-                    <label htmlFor="name_ja" className="block mb-1 text-[14px] md:text-[16px] md:mb-2">ストリング名(カナ)</label>
+                    <label htmlFor="name_ja" className="block mb-1 text-[14px] md:text-[16px] md:mb-2">ラケット名(カナ)</label>
                     <input type="text" name="name_ja" onChange={(e) => setInputNameJa(e.target.value)} className="border border-gray-300 rounded w-80 md:w-[380px] h-10 p-2 focus:outline-sub-green" />
                     {errors.name_ja.length !== 0 &&
                       errors.name_ja.map((message, i) => <p key={i} className="text-red-400">{message}</p>)
@@ -185,7 +180,7 @@ const RacketRegister: NextPage = () => {
                   </div>
 
                   <div className="mb-6">
-                    <label htmlFor="name_en" className="block mb-1 text-[14px] md:text-[16px] md:mb-2">ストリング名(アルファベット)</label>
+                    <label htmlFor="name_en" className="block mb-1 text-[14px] md:text-[16px] md:mb-2">ラケット名(アルファベット)</label>
                     <input type="text" name="name_en" onChange={(e) => setInputNameEn(e.target.value)} className="border border-gray-300 rounded w-80 md:w-[380px] h-10 p-2 focus:outline-sub-green" />
                     {errors.name_en.length !== 0 &&
                       errors.name_en.map((message, i) => <p key={i} className="text-red-400">{message}</p>)
@@ -215,15 +210,15 @@ const RacketRegister: NextPage = () => {
                     <input type="hidden" name="image_id" />
 
                     <div className="md:w-[100%] md:max-w-[380px]">
-                      <p className="mb-2 text-[14px] md:text-[16px]">選択中：{selectedGutImage ? selectedGutImage.title : 'デフォルト'}</p>
+                      <p className="mb-2 text-[14px] md:text-[16px]">選択中：{selectedRacketImage ? selectedRacketImage.title : 'デフォルト'}</p>
                       <div className="flex justify-between md:w-[100%] md:max-w-[380px]">
                         <div className="self-end">
-                          <button type="button" onClick={openModal} className="text-white font-bold text-[14px] w-[80px] h-6 rounded  bg-sub-green">変更</button>
+                          <button type="button" onClick={openModal} className="text-white font-bold text-[14px] w-[80px] h-6 rounded  bg-sub-green">選ぶ</button>
                         </div>
 
                         <div className="w-[100%] max-w-[200px] h-[120px] flex justify-center md:h-[160px] md:max-w-[] md:justify-end">
-                          {selectedGutImage
-                            ? <img src={`${baseImagePath}${selectedGutImage?.file_path}`} alt="" className="w-[100%] max-w-[120px] border md:max-w-[160px]" />
+                          {selectedRacketImage
+                            ? <img src={`${baseImagePath}${selectedRacketImage?.file_path}`} alt="" className="w-[100%] max-w-[120px] border md:max-w-[160px]" />
                             : <img src={`${baseImagePath}images/guts/default_gut.jpg`} alt="" className="w-[100%] max-w-[120px] border md:max-w-[160px]" />
                           }
                         </div>
@@ -244,7 +239,7 @@ const RacketRegister: NextPage = () => {
                     <IoClose size={48} />
                   </div>
 
-                  <form action="" onSubmit={searchGutImages} className="mb-[24px] md:flex md:mb-[40px]">
+                  <form action="" onSubmit={searchRacketImages} className="mb-[24px] md:flex md:mb-[40px]">
                     <div className="mb-6 md:mb-0 md:mr-[16px]">
                       <label htmlFor="name_ja" className="block mb-1 text-[14px] md:text-[16px] md:mb-2">画像 検索ワード</label>
                       <input type="text" name="name_ja" onChange={(e) => setInputSearchWord(e.target.value)} className="border border-gray-300 rounded w-80 md:w-[300px] h-10 p-2 focus:outline-sub-green" />
@@ -268,17 +263,17 @@ const RacketRegister: NextPage = () => {
                   <div className="w-[100%] max-w-[320px] md:max-w-[768px]">
                     <p className="text-[14px] mb-[16px] md:text-[16px] md:max-w-[640px] md:mx-auto">検索結果</p>
                     <div className="flex justify-between flex-wrap w-[100%] max-w-[320px] md:max-w-[640px] md:mx-auto md:justify-start">
-                      {searchedGutImages?.map(gutImage => (
+                      {searchedRacketImages?.map(racketImage => (
                         <>
-                          {/* ガット画像情報カード */}
-                          <div onClick={() => selectImage(gutImage)} className="bg-white p-2 rounded-lg w-[100%] max-w-[136px] hover:opacity-80 mb-6 hover:cursor-pointer md:[&:not(:last-child)]:mr-[24px]">
+                          {/* ラケット画像情報カード */}
+                          <div key={racketImage.id} onClick={() => selectImage(racketImage)} className="bg-white p-2 rounded-lg w-[100%] max-w-[136px] hover:opacity-80 mb-6 hover:cursor-pointer md:[&:not(:last-child)]:mr-[24px]">
                             <div className="w-[120px] mb-2">
-                              {gutImage.file_path && <img src={`${baseImagePath}${gutImage.file_path}`} alt="ストリング画像" className="w-[120px] h-[120px]" />}
+                              {racketImage.file_path && <img src={`${baseImagePath}${racketImage.file_path}`} alt="ストリング画像" className="w-[120px] h-[120px]" />}
                             </div>
 
-                            <p className="text-[14px] mb-1 md:text-[16px]">{gutImage.maker.name_ja}</p>
+                            <p className="text-[14px] mb-1 md:text-[16px]">{racketImage.maker.name_ja}</p>
 
-                            <p className="text-[14px] md:text-[16px]">{gutImage.title}</p>
+                            <p className="text-[14px] md:text-[16px]">{racketImage.title}</p>
                           </div>
                         </>
                       ))}
