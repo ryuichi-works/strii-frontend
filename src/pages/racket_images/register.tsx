0a4d95e5f3db7a19bbac1424d55ef164f95e3b7e
ryuichi-works <ryu1.works@gmail.com>
2023-12-31 02:50:35 +0900

    modify:#38:racket_image登録ページ、ページ上部のmarginの調整

--- src/pages/racket_images/register.tsx
+++ src/pages/racket_images/register.tsx
@@ -142,7 +142,7 @@ const GutImageRegister: NextPage = () => {
         {(isAuth || isAuthAdmin) && (
           <>
             <div className="container mx-auto mb-[48px]">
-              <div className="text-center mb-6 md:mb-[48px]">
+              <div className="text-center my-6 md:my-[32px]">
                 <PrimaryHeading text="ラケット画像登録" className="text-[18px] h-[20px] md:text-[20px] md:h-[22px]" />
               </div>
 
