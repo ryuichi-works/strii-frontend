1e54a10cd637e2b9eca5ff45259572125d46bcdd
ryuichi-works <ryu1.works@gmail.com>
2023-12-31 02:45:06 +0900

    modify:#38:racket関連ページ、ページ上部のmarginの調整
    PrimaryHeadingがアルファベットのところはitaric体に変更してある

--- src/pages/rackets/[id]/edit.tsx
+++ src/pages/rackets/[id]/edit.tsx
@@ -184,9 +184,9 @@ const RacketEdit: NextPage = () => {
         {isAuthAdmin && (
           <>
 
-            <div className="container mx-auto mb-8 w-screen pt-[24px] overflow-y-auto">
+            <div className="container mx-auto mb-8 w-screen overflow-y-auto">
 
-              <div className="text-center mb-6">
+              <div className="text-center my-6 md:my-[32px]">
                 <PrimaryHeading text="ラケット情報更新" className="text-[18px] h-[20px] md:text-[20px] md:h-[22px]" />
               </div>
 
--- src/pages/rackets/register.tsx
+++ src/pages/rackets/register.tsx
@@ -169,9 +169,9 @@ const RacketRegister: NextPage = () => {
         {isAuthAdmin && (
           <>
 
-            <div className="container mx-auto mb-8 w-screen pt-[24px] overflow-y-auto">
+            <div className="container mx-auto mb-8 w-screen overflow-y-auto">
 
-              <div className="text-center mb-6">
+              <div className="text-center my-6 md:my-[32px]">
                 <PrimaryHeading text="ラケット登録" className="text-[18px] h-[20px] md:text-[20px] md:h-[22px]" />
               </div>
 
