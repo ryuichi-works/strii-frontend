98ac052f5914c478b25dab6f3d9590aa5711dd5a
ryuichi-works <ryu1.works@gmail.com>
2024-01-07 05:53:50 +0900

    add:#95:gut_review一覧ページにページネーションを追加
    自作のしてあったPaginationコンポーネントを使用して実装

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,5 +1,5 @@
 import type { GutImage } from "@/pages/guts/register";
-import type { Gut } from "@/pages/reviews";
+import type { Gut, Review } from "@/pages/reviews";
 import type { Racket, RacketImage } from "@/pages/users/[id]/profile";
 import React from "react";
 
@@ -27,7 +27,7 @@ export type Paginator<T> = {
 
 type PaginationProps = {
   // ページネーションさせたい項目分だけpaginatorの型を増やして使う
-  paginator?: Paginator<Gut> | Paginator<Racket> | Paginator<GutImage> | Paginator<RacketImage>,
+  paginator?: Paginator<Gut> | Paginator<Racket> | Paginator<GutImage> | Paginator<Review>,
   //ページネイトリンクをクリックした時にデータをfetchするための関数が渡ってくる
   paginate: (url?: string) => Promise<void>,
   className?: string
