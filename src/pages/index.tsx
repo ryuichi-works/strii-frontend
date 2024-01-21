24afea41978b1ea845bf0ced29199d87ab38f04a
ryuichi-works <ryu1.works@gmail.com>
2024-01-22 05:01:07 +0900

    add:#115:heroに関してsm,md,lg,xlそれぞれのレスポンシブに対応したレイアウトの実装
    画像をImageコンポーネントで表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,28 +3,43 @@ import { Inter } from 'next/font/google'
 import Link from 'next/link';
 import SubHeading from '@/components/SubHeading';
 import TextUnderBar from '@/components/TextUnderBar';
+import HeroPcSize from '../../public/hero_pc_size.png';
+import HeroSpSize from '../../public/hero_sp_size.png';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      {/* <div className="container mx-auto"> */}
-      <div className=" mx-auto">
+      <div className="mx-auto">
         {/* hero section */}
-        <section className='min-h-[480px] md:h-[668px]'>
-          <div className='relative md:static md:flex '>
-            <div className='absolute top-[98px] flex flex-col items-center gap-[8px] flex-wrap w-full h-[290px] bg-black bg-opacity-50 md:static md:w-[60%] md:h-[670px] md:gap-[64px] md:justify-center md:shrink-[3]'>
-              <p className='text-white font-hero-sub text-[24px] w-full max-w-[290px] h-[144px] min-h-[144px]] mt-6 md:text-[32px] lg:text-[40px]  md:h-[180px] md:max-w-[374px] lg:max-w-[614px] md:mt-0'>
-                自分に合った<br className='lg:hidden'/>
+        <section className='container mx-auto h-[480px] sm:h-[358px] md:h-[430px] xl:h-[571px]'>
+          <div className='relative sm:static sm:flex lg:w-[1024px] xl:w-[1280px]'>
+            <div className='absolute top-[98px] flex flex-col items-center gap-[8px] flex-wrap w-full h-[290px] bg-black bg-opacity-60 sm:static sm:h-[358px] md:h-[430px] xl:h-[571px] lg:w-[62.5%] xl:w-[60%]'>
+              <p className='text-white font-hero-sub text-[24px] md:text-[28px] lg:text-[32px] xl:text-[40px] w-full max-w-[290px] sm:max-w-[260px] md:max-w-[312px] lg:max-w-[518px] xl:max-w-[648px] sm:mb-8  min-h-[144px] mt-6 md:mt-[48px] xl:mt-[72px] md:mb-[48px] xl:mb-[56px] '>
+                自分に合った<br className='lg:hidden' />
                 ストリング・ガット<br />
-                を見つけるためのサイト<br />
+                を見つけるための<br className='hidden sm:block md:hidden' />サイト<br />
                 そうそれが<br />
               </p>
-              <p className='w-full max-w-[326px] text-right text-white text-[28px] font-bold italic md:text-[32px] lg:text-[45px]'>Strii(ストリー)</p>
-            </div>
-            <div className='w-full h-[480px] bg-green-200 absolute top-0 left-0 -z-10 md:static md:w-[40%] md:h-[670px] md:shrink-[2]'>
 
+              <p className='w-full max-w-[326px] sm:max-w-[260px] md:max-w-[312px] lg:max-w-[518px] text-right text-white text-[28px] sm:text-[32px] md:text-[36px] lg:text-[45px] font-bold italic'>Strii(ストリー)</p>
+            </div>
+
+            <div className='w-full h-[480px] absolute top-0 left-0 -z-10 sm:static sm:h-auto lg:w-auto'>
+              <div className='flex justify-center  relative'>
+                <Image
+                  src={HeroPcSize}
+                  alt="Vercel Logo"
+                  className="hidden sm:block sm:w-[320px] md:w-[384px] xl:w-[512px]"
+                />
+                
+                <Image
+                  src={HeroSpSize}
+                  alt="Vercel Logo"
+                  className="sm:hidden"
+                />
+              </div>
             </div>
           </div>
         </section>
@@ -46,6 +61,7 @@ export default function Home() {
                 レビューを見て自分に合った<br className='md:hidden' />
                 ストリング・ガットを探そう!!
               </p>
+
               <div className='w-full max-w-[320px] bg-white rounded-2xl border border-gray-300 shadow-[0_2px_2px_rgba(0,0,0,0.3)] md:max-w-[768px] md:text-[18px]'>
                 <p className='text-4 p-4'>
                   テニス歴や年齢の近い人、<br className='md:hidden' />
@@ -62,6 +78,7 @@ export default function Home() {
                 他の仲間のストリング・ガット<br className='md:hidden' />
                 探しに貢献しよう！！
               </p>
+
               <div className='w-full max-w-[320px] bg-white rounded-2xl border border-gray-300 shadow-[0_2px_2px_rgba(0,0,0,0.3)]  md:max-w-[768px] md:text-[18px]'>
                 <p className='text-4 p-4'>
                   ストリング選びに悩んでいる他の仲間の助けになります
@@ -83,9 +100,10 @@ export default function Home() {
               <p className='font-top-yumincho text-[18px] font-[500] mb-4 w-[272px] md:w-[768px] md:text-center md:text-[22px]'>
                 過去の装備と比べて、より良いセッティングを探そう！！！
               </p>
+
               <div className='w-full max-w-[320px] bg-white rounded-2xl border border-gray-300 shadow-[0_2px_2px_rgba(0,0,0,0.3)] md:max-w-[768px] md:text-[18px]'>
                 <p className='text-4 p-4'>
-                  ストリング・ガットの太さ、<br className='md:hidden'/>テンション、張った日など
+                  ストリング・ガットの太さ、<br className='md:hidden' />テンション、張った日など
                   詳細に記録し、後で見比べることができます。
                 </p>
               </div>
