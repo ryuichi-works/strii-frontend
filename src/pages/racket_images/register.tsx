e3495129c381b4e8f7aba674a8b598ebe987a9e6
ryuichi-works <ryu1.works@gmail.com>
2024-04-24 14:55:27 +0900

    add:#127:racket_images関連ページのtitleタグを設定
    ページの役割を表す内容でtitileタグを設定した。

--- src/pages/racket_images/register.tsx
+++ src/pages/racket_images/register.tsx
@@ -5,6 +5,7 @@ import Cookies from "js-cookie";
 
 import { useContext, useEffect, useRef, useState } from "react";
 import { useRouter } from "next/router";
+import Head from 'next/head';
 import { AuthContext } from "@/context/AuthContext";
 
 import AuthCheck from "@/components/AuthCheck";
@@ -172,6 +173,10 @@ const GutImageRegister: NextPage = () => {
       <AuthCheck>
         {(isAuth || isAuthAdmin) && (
           <>
+            <Head>
+              <title>ラケット画像提供</title>
+            </Head>
+
             <div className="container mx-auto mb-[48px]">
               <FlashMessage
                 flashMessage={'画像提供受付ました、ご協力ありがとうございます。'}
